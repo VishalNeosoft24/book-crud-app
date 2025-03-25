d9f2f18597ec00be71fd6266accc0669ac9e4593
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2025-03-25 19:18:16 +0530

    add link for detail and delete

--- src/bookcard/BookCard.jsx
+++ src/bookcard/BookCard.jsx
@@ -5,7 +5,12 @@ function truncateText(text, maxLength) {
   return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
 }
 
-export default function BookCard({ book }) {
+export default function BookCard({ book, bookDelete }) {
+function name(params) {
+  
+}
+
+  
   return (
     <div className="card h-100 shadow-sm">
       <img
@@ -15,18 +20,28 @@ export default function BookCard({ book }) {
         style={{ height: "200px", objectFit: "cover" }}
       />
       <div className="card-body d-flex flex-column">
+        <h5 className="card-id">{book.id}</h5>
         <h5 className="card-title">{book.title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{book.author}</h6>
         <p className="card-text flex-grow-1" title={book.description}>
           {truncateText(book.description, 50)}
         </p>
         <div className="d-flex justify-content-between">
-          <Link to="#" className="btn btn-outline-primary">
+          <Link to={`/book/${book.id}`} className="btn btn-outline-primary">
+            Detail
+          </Link>
+          <Link
+            to={`/edit-book/${book.id}`}
+            className="btn btn-outline-primary"
+          >
             Edit
           </Link>
-          <Link to="#" className="btn btn-outline-danger">
-            Delete
-          </Link>
+          <button
+            className="btn btn-outline-danger"
+            onClick={() => bookDelete(book.id)}
+          >
+            DELETE
+          </button>
         </div>
       </div>
     </div>
